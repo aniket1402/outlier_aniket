0a8f349ac5c55f3e1ff75a28a8028bc1dc5ade8c
Aniket Gupta <aniketgupta1495@gmail.com>
2020-02-14 15:53:53 +0530

    4th commit

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'outlier_aniket',
   packages = ['outlier_aniket'],
-  version = '0.4',
+  version = '0.5',
   license='MIT',
   description = 'Outlier Removal using Z-score or IQR',
   author = 'Aniket Gupta',
   author_email = 'aniketgupta1495@gmail.com',
   url = 'https://github.com/aniket1402/outlier_aniket.git',
-  download_url = 'https://github.com/aniket1402/outlier_aniket/archive/v_04.tar.gz',
+  download_url = 'https://github.com/aniket1402/outlier_aniket/archive/v_05.tar.gz',
   keywords = ['Outlier Removal', 'Row Removal'],
   install_requires=[
           'numpy',
@@ -19,6 +19,11 @@ setup(
     'Intended Audience :: Developers',
     'Topic :: Software Development :: Build Tools',
     'License :: OSI Approved :: MIT License',
-    'Programming Language :: Python >= 3',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
   ],
 )
